84a2d99068fd53e0a871af6685b89bec2202582d
Alex <alex.rr1495@gmail.com>
2023-08-15 12:16:39 -0600

    Second commit

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
 import repositories from '../data/repositories.js'
 
 const RepositoryList = () => {
     return (
-      <View>
-          {repositories.map(repo =>(
-            <View key={repo.id}>
-                <Text>{repo.id}</Text>
-                <Text>{repo.fullname}</Text>
+      <FlatList 
+        data = {repositories}
+        ItemSeparatorComponent={() => <Text></Text>}
+        renderItem={({item: repo}) => (
+            <View key={repo.id} style={{ padding: 20, paddingBottom: 5, paddingTop: 5}}>
+                <Text>id: {repo.id}</Text>
+                <Text>Fullname: {repo.fullname}</Text>
+                <Text>Description: {repo.description}</Text>
+                <Text>Language: {repo.language}</Text>
+                <Text>Stars: {repo.stargazersCount}</Text>
+                <Text>Forks: {repo.forksCount}</Text>
+                <Text>Review: {repo.reviewCount}</Text>
+                <Text>Rating: {repo.ratingAverage}</Text>
             </View>
-          ))}
-      </View>
+        )}
+      /> 
     )
 }
-
 export default RepositoryList
--- src/data/repositories.js
+++ src/data/repositories.js
@@ -1,9 +1,57 @@
 export default[
 {
- id: 'midu.mx',
- fullname: 'midudev/midudev'
-
-}
-
-
+    id: 'midu.mx',
+    fullname: 'midudev/midudev',
+    description: 'Blog de noticias sobre desarrollo',
+    language: 'HTML',
+    forksCount: 3123,
+    stargazersCount: 52869,
+    ratingAverage: 0,
+    reviewCount: 0,
+    ownerAvatarUrl: 'https://avengers.githubusercontent.com/u/181920'
+},
+{
+    id: 'midu2.mx',
+    fullname: 'midudev2/midudev',
+    description: 'Blog2 de noticias sobre desarrollo',
+    language: 'JAVA',
+    forksCount: 3124,
+    stargazersCount: 52870,
+    ratingAverage: 0,
+    reviewCount: 0,
+    ownerAvatarUrl: 'https://avengers.githubusercontent.com/u/181925'
+},
+{
+    id: 'midu3.mx',
+    fullname: 'midudev3/midudev',
+    description: 'Blog3 de noticias sobre desarrollo',
+    language: 'REACT',
+    forksCount: 3125,
+    stargazersCount: 52871,
+    ratingAverage: 0,
+    reviewCount: 0,
+    ownerAvatarUrl: 'https://avengers.githubusercontent.com/u/181930'
+},
+{
+    id: 'midu4.mx',
+    fullname: 'midudev4/midudev',
+    description: 'Blog4 de noticias sobre desarrollo',
+    language: 'ANDROID',
+    forksCount: 3126,
+    stargazersCount: 52872,
+    ratingAverage: 0,
+    reviewCount: 0,
+    ownerAvatarUrl: 'https://avengers.githubusercontent.com/u/181935'
+ },
+ {
+    id: 'midu5.mx',
+    fullname: 'midudev5/midudev',
+    description: 'Blog5 de noticias sobre desarrollo',
+    language: 'IOS',
+    forksCount: 3127,
+    stargazersCount: 52873,
+    ratingAverage: 0,
+    reviewCount: 0,
+    ownerAvatarUrl: 'https://avengers.githubusercontent.com/u/181940'
+ },
 ]
